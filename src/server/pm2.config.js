5b3c73fe6649c7a4affd80f9ee4900ade075527a
xiongqi <xiongqi.me@foxmail.com>
2020-03-04 14:22:30 +0800

    fix: pm2 config

--- src/server/pm2.config.js
+++ src/server/pm2.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
-
+const path = require("path");
+const { WEB_ROOT } = process.env;
 module.exports = {
   apps: [
     {
-      name: 'chat',
-      script: path.resolve(__dirname, './server.js'),
-      watch: [path.resolve(__dirname)],
-      ignore_watch: ['**/logs', '**/node_modules'],
+      name: "chat",
+      script: path.resolve(__dirname, "./server.js"),
+      watch: true,
+      ignore_watch: ["**/logs", "**/node_modules"],
       env: {
-        NODE_ENV: 'prod',
+        NODE_ENV: "prod"
       },
-      out_file: path.resolve(__dirname, './logs/chat-out.log'),
-      error_file: path.resolve(__dirname, './logs/chat-error.log'),
-    },
-  ],
+      out_file: path.resolve(WEB_ROOT, "./logs/chat/out.log"),
+      error_file: path.resolve(WEB_ROOT, "./logs/chat/error.log")
+    }
+  ]
 };
